f7fb893af2e345dd5ad37d5734487e927bcfbbe4
DylanTHK <dylantanhk@gmail.com>
2022-05-31 18:25:07 +0800

    Added slider input to index.html Added function to adjust gridsize in script.js based on slider input Added variable in style.css to allow for auto adjustment of col width

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+const gridSize = document.getElementById("gridSize");
+const gridText = document.getElementById("grid-size-text")
+
+gridSize.onchange = (element) => createGrid(element.target.value);
+
 function createGrid(n) {
+    gridText.textContent = `${n} x ${n}`;
+    updateCSS(n);
+    const resetGrid = document.getElementById("sketch-pad");
+    resetGrid.innerHTML = " ";
+
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
             let div = document.createElement("div");
             div.id = `${i}${j}`;
-            div.textContent = `${i}${j}`;
+            div.className = "grid-border";
+            // div.textContent = `${i}${j}`;
             let sketchPad = document.querySelector("#sketch-pad");
             sketchPad.appendChild(div);
         }
     }
+    console.log("done");
 }
 
-createGrid(3);
+function updateCSS(rows) {
+    const root = document.querySelector(":root");
+    root.style.setProperty("--rows", rows);
+}
+
+createGrid(gridSize.value);
