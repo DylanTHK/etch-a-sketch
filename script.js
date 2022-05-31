96259a9ce6407f61258289ccdaecddd2d8eb3555
DylanTHK <dylantanhk@gmail.com>
2022-05-31 18:33:15 +0800

    Added comments for script.js set max value of slider to 64 and value to 16

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const gridText = document.getElementById("grid-size-text")
 
 gridSize.onchange = (element) => createGrid(element.target.value);
 
+// function to add number of cells based on slider input
 function createGrid(n) {
+    // update grid size for user visual
     gridText.textContent = `${n} x ${n}`;
     updateCSS(n);
+    // to reset the grid for every change in slider input
     const resetGrid = document.getElementById("sketch-pad");
     resetGrid.innerHTML = " ";
 
@@ -22,6 +25,7 @@ function createGrid(n) {
     console.log("done");
 }
 
+// function to update number of columns in style.css
 function updateCSS(rows) {
     const root = document.querySelector(":root");
     root.style.setProperty("--rows", rows);
