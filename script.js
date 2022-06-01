4649ef5153a193ae9fb3bdf5cb7d1206171c8c77
DylanTHK <dylantanhk@gmail.com>
2022-06-01 16:44:37 +0800

    updated id naming in index.html (camelCase) Added variable names for button elements in in html

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-const gridSize = document.getElementById("gridSize");
+const gridSize = document.getElementById("gridSize"); 
 const gridText = document.getElementById("grid-size-text")
+const grids = document.getElementsByClassName("grids");
 
-gridSize.onchange = (element) => createGrid(element.target.value);
+gridSize.onchange = (element) => initialiseGrid(element.target.value);
+grids.onclick = (cell) => updateColour(cell);
 
 // function to add number of cells based on slider input
-function createGrid(n) {
+function initialiseGrid(n) {
     // update grid size for user visual
     gridText.textContent = `${n} x ${n}`;
     updateCSS(n);
     // to reset the grid for every change in slider input
-    const resetGrid = document.getElementById("sketch-pad");
-    resetGrid.innerHTML = " ";
+    resetGrid();
 
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
             let div = document.createElement("div");
-            div.id = `${i}${j}`;
-            div.className = "grid-border";
-            // div.textContent = `${i}${j}`;
+            div.className = "grids";
+            // div.addEventListener();
             let sketchPad = document.querySelector("#sketch-pad");
             sketchPad.appendChild(div);
         }
     }
-    console.log("done");
 }
 
 // function to update number of columns in style.css
@@ -31,4 +30,19 @@ function updateCSS(rows) {
     root.style.setProperty("--rows", rows);
 }
 
-createGrid(gridSize.value);
+function resetGrid() {
+    const resetGrid = document.getElementById("sketch-pad");
+    resetGrid.innerHTML = " ";
+}
+
+
+// function to change colour
+function updateColour(element) {
+    element.target.style.backgroundColor = "black";
+}
+
+
+window.onload = () => {
+    initialiseGrid(gridSize.value);
+}
+
